1d960bcbbb31e445f4d74e6f4499cead3690e59b
Ehsan Langroudi (kendrick) <ehsanlangroudi.kk@gmail.com>
2022-06-29 17:32:14 +0430

    aw yup :U

---
+++ src/Days/D02-DO3/D02-3.js
@@ -0,0 +1,9 @@
+export default function D01() {
+
+    return (
+        <div>
+            <h1>based on these 2 challenges the existence of these files is the proof that iv did the exercises :I</h1>
+            
+        </div>
+    )
+}
